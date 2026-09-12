da5976d017050f489685eef5ef25dc90279d7405
onionloop <125755906+onionloop@users.noreply.github.com>
2026-09-12 22:58:29 +0530

    Refactor frontend to integrate Clerk authentication and clean up server code

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
 app.use(express.json());
-app.use(cors({origin:FRONTEND_URL , credentials:true}));
+app.use(cors({ origin: FRONTEND_URL, credentials: true }));
 app.use(clerkMiddleware());
 
 app.get("/health", (req, res) => {
